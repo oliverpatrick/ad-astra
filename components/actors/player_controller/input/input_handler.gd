0f274114f0aeb8f3eacf6a0fab13b436accc741a
KieranLProctor <kieran.lee.proctor@gmail.com>
2026-04-02 04:55:06 +0100

    fix: camera rotation now dictates movement direction when moving forward

--- components/actors/player_controller/input/input_handler.gd
+++ components/actors/player_controller/input/input_handler.gd
@@ -1,6 +1,8 @@
 class_name InputHandler 
 extends Node
 
+@onready var _pivot_y: Node3D = $"../CameraMount/PivotY"
+
 func handle_inputs() -> InputPackage:
 	var new_input = InputPackage.new()
 	
@@ -10,6 +12,7 @@ func handle_inputs() -> InputPackage:
 	if Input.is_action_just_pressed("crouch"):
 		new_input.actions.append("crouch")
 		
+	new_input.camera_basis = _pivot_y.global_transform.basis
 	new_input.input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	if new_input.input_direction != Vector2.ZERO:
 		new_input.actions.append("run")
